8a1dd7708169400da6a57141f383d764a805160f
Calvin Mitchell <dev@clmitchell.net>
2015-08-14 15:29:55 -0400

    v0.3.0: Successfully created Map Tab code!

---
+++ src/net/clm/history/pages/pgMap.clj
@@ -0,0 +1,22 @@
+(ns net.clm.history.pages.pgAdd
+    (:require [hiccup.core :as hcore]
+      [hiccup.page :as hic]
+      [net.clm.history.pages.snippets :as snippets]))
+
+
+(defn page
+      "Index Page for History of the World."
+      []
+      (hcore/html
+        (hic/html5
+        (snippets/head "add" "Dot Net II: History of the World: Add")
+         [:body {:id "AddEvent"}
+          [:h1 {:class "title"} "History of the World"]
+          [:div {:id "tabsF"} (snippets/tabsf)]
+          [:div {:id "content"}
+           [:iframe {:frameborder "0",
+                     :marginheight "0",
+                     :marginwidth "0",
+                     :src "https://docs.google.com/spreadsheet/embeddedform?formkey=dHU1SWpjQ1A2cUVkSlBWVC1vNGxzSWc6MA",
+                     :width "100%",
+                     :height "1325"} "Loading"]]])))
