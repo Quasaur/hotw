dac5e64f23a1305efc7ccf3bd1ea74866886a85c
Calvin Mitchell <dev@clmitchell.net>
2015-08-15 22:43:34 -0400

    v0.4.3: downloaded missing js/live.js from live server (not included in master branch either...corrected).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clm.history "0.4.0-SNAPSHOT"
+(defproject net.clm.history "0.4.3-SNAPSHOT"
   :main net.clm.history.handler
   :description "A rewrite of my History of the World in Clojure"
   :url "http://history.clmitchell.net"
