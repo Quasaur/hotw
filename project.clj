20d5f34afdd56bb60caea84c21cd9d91ec7e1f56
Calvin Mitchell <dev@clmitchell.net>
2015-09-05 00:30:03 -0400

    v0.10.9: Indented pgLearn.clj properly.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clm.history "0.10.9-SNAPSHOT"
+(defproject net.clm.history "0.10.10-SNAPSHOT"
   :main net.clm.history.handler
   :aot [net.clm.history.handler]
   :description "A rewrite of my History of the World in Clojure"
