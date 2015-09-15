eee3e06ceadd662410245c165679648fc25bae74
Calvin Mitchell <dev@clmitchell.net>
2015-09-14 21:41:53 -0400

    v1.0.7: Updated doc folder using Codox.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clm.history "1.0.6"
+(defproject net.clm.history "1.0.7"
   :main net.clm.history.handler
   :aot [net.clm.history.handler]
   :description "A rewrite of my History of the World in Clojure"
