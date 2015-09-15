881a63e3c2b96c283d094033760ee87be9b93ede
Calvin Mitchell <dev@clmitchell.net>
2015-09-15 09:44:37 -0400

    v1.1.2: (1) Updated neocons to 3.1.0-rc1 (2) imported neocons test directory (now to just get the tests to run!).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clm.history "1.1.1"
+(defproject net.clm.history "1.1.2"
   :main net.clm.history.handler
   :aot [net.clm.history.handler]
   :description "A rewrite of my History of the World in Clojure"
@@ -8,7 +8,7 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [clojurewerkz/neocons "3.0.0"]]
+                 [clojurewerkz/neocons "3.1.0-rc1"]]
   :plugins [[lein-ring "0.9.6"]
             [codox "0.8.13"]]
   :ring {:handler net.clm.history.handler/app
