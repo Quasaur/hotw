3e93f2cc2548453fb37b5f0a9b27778bfe89c22b
Calvin Mitchell <dev@clmitchell.net>
2015-09-16 02:56:14 -0400

    v1.1.5: Progress being made on Events table. NOTE: will have to rebuilt Events in Neo4j db to include Place (maybe...need to do research).

--- src/net/clm/history/tablegen.clj
+++ src/net/clm/history/tablegen.clj
@@ -1,3 +1,11 @@
 (ns ^{:author "Calvin L. Mitchell"
       :doc "Routines to turn the Neo4j JSON data into an HTML5 table."}
     net.clm.history.tablegen)
+
+(defn- neo2map [ejsondata])
+
+(defn- ehiccup [events-neomap])
+
+(defn events-hiccup [njson-data]
+  (let [eneo4map (neo2map njson-data)]
+    (ehiccup eneo4map)))
